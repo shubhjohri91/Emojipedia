17d36ca16e47fdd543c5165060672257a67afc06
Shubh Johri <55338440+shubhjohri91@users.noreply.github.com>
2020-10-15 15:00:03 -0700

    Files added

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😉",
+    name: "Winking Face",
+    meaning:
+      "A yellow face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic."
+  },
+  {
+    id: 5,
+    emoji: "🤪",
+    name: "Zany Face",
+    meaning:
+      "A smiley making a silly face. A yellow face with a big grin and wide, white eyes, one larger than the other and in a wild, cockeyed expression. Many platforms, including Apple, depict its tongue stuck out and head tilted; others feature a full-toothed grin, giving it a more unhinged appearance."
+  },
+  {
+    id: 6,
+    emoji: "🤬",
+    name: "Face with Symbols on Mouth",
+    meaning:
+      "An angry-red face with a black bar and white grawlixes covering its mouth, indicating it's swearing or being vulgar. Grawlixes are typographical symbols (e.g., @#$%&!) representing swear words or obscenities. Often used to convey an outburst of anger, frustration, or rage."
+  }
+];
+
+export default emojipedia;
